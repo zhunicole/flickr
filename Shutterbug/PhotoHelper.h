1ed255dce931882f65a34f7fab8cfc8d57a57c09
Nicole Zhu <nicolez@stanford.edu>
2014-05-21 02:32:24 -0700

    added recents

---
+++ Shutterbug/PhotoHelper.h
@@ -0,0 +1,13 @@
+//
+//  PhotoHelper.h
+//  TopPlacesPhotos
+//
+//  Created by Nicole Zhu on 5/21/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoHelper : NSObject
+
+@end
---
+++ Shutterbug/PhotoHelper.m
@@ -0,0 +1,13 @@
+//
+//  PhotoHelper.m
+//  TopPlacesPhotos
+//
+//  Created by Nicole Zhu on 5/21/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import "PhotoHelper.h"
+
+@implementation PhotoHelper
+
+@end
